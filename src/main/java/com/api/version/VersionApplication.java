2c9592a75e5d13a3649dde2e5c95856f9c396c6f
Kauan Reis <kauanreisgomes123@gmail.com>
2022-10-01 16:45:57 -0300

    Importação funcoes Kauan

--- src/main/java/com/api/version/VersionApplication.java
+++ src/main/java/com/api/version/VersionApplication.java
@@ -8,12 +8,22 @@ import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.functions.dao.Query;
+import com.functions.models.Mail;
+import com.functions.models.Objeto;
+
+
 
 @SpringBootApplication
 public class VersionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VersionApplication.class, args);
+		Objeto item = new Objeto();
+		Query q = new Query("jdbc:mysql://localhost:3306/api?useTimezone=true&serverTimezone=America/Sao_Paulo", "api", "123456789");
+		q.isOpen(true);
+		System.out.println(q.Count("SELECT count(*) FROM tb_versao"));
+		q.isOpen(false);
+		//SpringApplication.run(VersionApplication.class, args);
 	}
 
 	
